e32429ad15bebf0d02c561ccad901a37be20aa87
kubraqasimi <kubra.qasimi@studytafensw.edu.au>
2023-08-26 12:02:20 +1000

    Updated

--- src/test/java/tek/capstone/framework/runner/TestRunner.java
+++ src/test/java/tek/capstone/framework/runner/TestRunner.java
@@ -9,7 +9,7 @@ import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-		tags = "@orderCancellation", 
+		tags = "@Smoke", 
 		features = ("classpath:features"), 
 		glue = ("tek.capstone.framework"), 
 		dryRun = false, 
